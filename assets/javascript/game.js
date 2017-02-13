d76592659dc72a85ec3f60d8424fb3624ca557fb
Katharina Fuetterer <Katharina Fuetterer>
2017-02-12 22:24:10 -0500

    Update 02/12/2017 1024

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,36 @@
   
 	var lives = 10;
+	var letter = ["_ ","_ ","_ ","_ ","_ ","_ "];
+    var z = ["_ ","_ ","_ ","_ ","_ ","_ "];
 
     document.onkeyup = function(event) {
         var userGuess = event.key;
-        var letter = ["1","2","3","4","5","6"];
         var word = "puzzle";
+    
 
-	        for (var i=0; i < word.length; i++) {
+
+        	for (var i = 0; i < word.length; i++) {
+
+        		if (typeof stuff === 'undefined') {
+        			var stuff = letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5];
+        		}
+   
+        		z[i] = stuff.substring((i * 2), (i * 2) + 2);
+    
+	        };
+
+	        for (var i = 0; i < word.length; i++) {
 
 	        	if (word.indexOf(userGuess, i) === i) {
 	        		letter[i] = userGuess + " ";
 	        	}
 	        	else {
-	        		letter[i] = "_ ";
+	        		letter[i] = z[i];
 	        	}
 	        };
 
-       	var html = "<p>" + letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5] + "</p><p>Lives: " + lives + "</p>";
+	    stuff = letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5];
+
+       	var html = "<p>" + stuff + "</p><p>Lives: " + lives + "</p>";
        	document.querySelector("#game").innerHTML = html;
     }
