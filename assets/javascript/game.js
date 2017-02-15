8d84f0391266027b2e6cd2c023e261c550758dd5
Katharina Fuetterer <Katharina Fuetterer>
2017-02-14 20:21:42 -0500

    Update 02/14/2017 821

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,37 +9,30 @@
     document.onkeyup = function(event) {
         var userGuess = event.key;
         var word = ["puzzle", "hobbit", "knight", "future", "potion", "newton", "random", "abacus"];
-
         var q = 0;
     
         	for (var i = 0; i < word[q].length; i++) {
-
         		if (typeof stuff === 'undefined') {
         			var stuff = letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5];
-        		}
-   
-        		z[i] = stuff.substring((i * 2), (i * 2) + 2);
-    
+        			} z[i] = stuff.substring((i * 2), (i * 2) + 2);
 	        };
 
 	        for (var i = 0; i < word.length; i++) {
-
 	        	if (word[q].indexOf(userGuess, i) === i) {
 	        		letter[i] = userGuess + " ";
-	        	}
-	        	else {
+	        	} else {
 	        		letter[i] = z[i];
 	        		y++;
 	        	}
 	        };
 
-	        if (y = word.length) {
+	  		if (y = word.length) {
 	        	lives--;
 	        };
 
 	    stuff = letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5];
 
-       	var html = "<p>" + stuff + "</p><p>Lives: " + lives + "</p><p>Wins: " + wins + "</p>";
+       	var html = "<p>" + stuff + "</p><p>Letters Guessed: " + userGuess.toUpperCase() + "</p><p>Lives: " + lives + "</p><p>Wins: " + wins + "</p>";
        	document.querySelector("#game").innerHTML = html;
 
 	        for (var i = 0; i < word[q].length; i++) {
@@ -61,14 +54,16 @@
 			    var z = ["_ ","_ ","_ ","_ ","_ ","_ "];	
 		    }
 
-
 		 	if (lives === 0) {
 		  		var htmll = "<h2>Game Over</h2>";
 		  		document.querySelector("#gameover").innerHTML = htmll;
+
+		  		var htmlll = "<div style='text-align: center;'><button style='border-radius: 12px; background-color: #ffffff; border-color: #bbbbbb; border-width: 2px; border-style: solid; color: #555555; font-size: 20px; padding: 15px 32px; font-family: 'Josephin Sans', sans-serif;'>Restart</button>"
+       			document.querySelector("#button").innerHTML = htmlll;
 		  	}
 
 		  	ResetGlobalVariables();
-
+		  	//when button is pushed resetglobalvariables and run function again.
     }
 
 
