8d557cac9698080cbf2e00724a05faad0ce2ba70
Katharina Fuetterer <Katharina Fuetterer>
2017-02-15 20:06:47 -0500

    Update 02/15/2017 806

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,104 @@
-  
+
+
 	var lives = 20;
 	var wins = 0;
 	var t = 0;
-	var y = 0;
+	var y = 6;
 	var letter = ["_ ","_ ","_ ","_ ","_ ","_ "];
     var z = ["_ ","_ ","_ ","_ ","_ ","_ "];
+    var q = 0;
+    var guesses = "";
 
-    document.onkeyup = function(event) {
-        var userGuess = event.key;
-        var word = ["puzzle", "hobbit", "knight", "future", "potion", "newton", "random", "abacus"];
-        var q = 0;
     
+	window.onload = function() {
+		var html = "<p>" + letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5]; + "</p><p>Letters Guessed</p><p>" + "0" + "</p><p>Lives: " + lives + "</p><p>Wins: " + wins + "</p>";
+	    document.querySelector("#game").innerHTML = html;
+	}
+
+    document.onkeyup = function Game(event) {
+        var userGuess = event.key;
+        var word = ["puzzle", "hobbit", "knight", "future", "potion", "newton"];
+
+    		if (t === 6) {
+	  			return;
+    		};
+
+    		if (lives === 0) {
+    			return;
+    		};
+
+    		document.onkeyup;
+
         	for (var i = 0; i < word[q].length; i++) {
         		if (typeof stuff === 'undefined') {
         			var stuff = letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5];
         			} z[i] = stuff.substring((i * 2), (i * 2) + 2);
 	        };
 
-	        for (var i = 0; i < word.length; i++) {
+	        for (var i = 0; i < word[q].length; i++) {
 	        	if (word[q].indexOf(userGuess, i) === i) {
 	        		letter[i] = userGuess + " ";
+	        		t++;
+	        		y--;
 	        	} else {
 	        		letter[i] = z[i];
-	        		y++;
 	        	}
 	        };
 
-	  		if (y = word.length) {
+	  		if (y === 6) {
 	        	lives--;
 	        };
 
+	    if (guesses === "") {
+	    	guesses = userGuess.toUpperCase();
+	    }
+	    else {
+	    	guesses = guesses + " " + userGuess.toUpperCase();
+	    }
+
+	    y = 6;
+
 	    stuff = letter[0] + letter[1] + letter[2] + letter[3] + letter[4] + letter[5];
 
-       	var html = "<p>" + stuff + "</p><p>Letters Guessed: " + userGuess.toUpperCase() + "</p><p>Lives: " + lives + "</p><p>Wins: " + wins + "</p>";
+	    if (t === 6) {
+
+	  		wins++;
+	  		q++;
+	  		t = 0;
+			y = 6;
+			letter = ["_ ","_ ","_ ","_ ","_ ","_ "];
+			z = ["_ ","_ ","_ ","_ ","_ ","_ "];
+			guesses = "";
+
+		html = "<p>" + stuff + "</p><p>Letters Guessed</p><p>" + guesses + "</p><p>Lives: " + lives + "</p><p>Wins: " + wins + "</p>";
        	document.querySelector("#game").innerHTML = html;
 
-	        for (var i = 0; i < word[q].length; i++) {
-	  			if (z[i] != "_ ") {
-	  				t++;
-	  			}
-	  		}
-	  		if (t = word[q].length) {
-	  			q++;
-	  			wins++;
-	  		}
+	  	};
 
-	  		function ResetGlobalVariables() {
-				var lives = 20;
-				var wins = 0;
-				var t = 0;
-				var y = 0;
-				var letter = ["_ ","_ ","_ ","_ ","_ ","_ "];
-			    var z = ["_ ","_ ","_ ","_ ","_ ","_ "];	
-		    }
+       	html = "<p>" + stuff + "</p><p>Letters Guessed</p><p>" + guesses + "</p><p>Lives: " + lives + "</p><p>Wins: " + wins + "</p>";
+       	document.querySelector("#game").innerHTML = html;
 
-		 	if (lives === 0) {
-		  		var htmll = "<h2>Game Over</h2>";
-		  		document.querySelector("#gameover").innerHTML = htmll;
 
-		  		var htmlll = "<div style='text-align: center;'><button style='border-radius: 12px; background-color: #ffffff; border-color: #bbbbbb; border-width: 2px; border-style: solid; color: #555555; font-size: 20px; padding: 15px 32px; font-family: 'Josephin Sans', sans-serif;'>Restart</button>"
-       			document.querySelector("#button").innerHTML = htmlll;
-		  	}
+	   		if (lives === 0) {
+		  	var htmll = "<h2>Game Over</h2>";
+		  	document.querySelector("#gameover").innerHTML = htmll;
 
-		  	ResetGlobalVariables();
-		  	//when button is pushed resetglobalvariables and run function again.
+		  	var htmlll = "<div style='text-align: center;'><button onclick='buttonPress()' style='border-radius: 12px; background-color: #ffffff; border-color: #bbbbbb; border-width: 2px; border-style: solid; color: #555555; font-size: 20px; padding: 15px 32px; font-family: 'Josephin Sans', sans-serif;'>Restart</button>";
+       		document.querySelector("#button").innerHTML = htmlll;
+       		}
     }
+
+   // buttonPress() {
+
+	//	lives = 20;
+		//wins = 0;
+	//	t = 0;
+	//	y = 0;
+	//	letter = ["_ ","_ ","_ ","_ ","_ ","_ "];
+	//	z = ["_ ","_ ","_ ","_ ","_ ","_ "];
+	//	q = 0; 
+		//when button is pushed resetglobalvariables and run function again.
+//	};
+
 
 
